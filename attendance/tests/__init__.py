8e8c2e66496786bcd95033c00aaf65578522655c
CyberArcenal <148480765+CyberArcenal@users.noreply.github.com>
2026-04-22 03:03:22 +0800

    clean

--- attendance/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from .student_attendance import StudentAttendance
-from .teacher_attendance import TeacherAttendance
-from .attendance_summary import StudentAttendanceSummary
-from .holiday import Holiday
